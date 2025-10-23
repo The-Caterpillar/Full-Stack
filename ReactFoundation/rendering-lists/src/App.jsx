3f7f7250cd64d07b9408f349a32e5577219087f9
Saraswati Tiwari <saraswati99Tiwari@gmail.com>
2025-10-23 23:27:54 +0530

    rendering lists in react

---
+++ ReactFoundation/rendering-lists/src/App.jsx
@@ -0,0 +1,29 @@
+import List from "./Lists"
+function App() {
+      
+  const fruits =
+        [
+            {name: 'apple', calories: 95},
+            {name: 'orange', calories: 45},
+            {name: 'banana', calories: 105},
+            {name: 'pineapple', calories: 37},
+            {name: 'peach', calories: 159}
+        ];
+
+  const vegetables =
+        [
+            {name: 'potato', calories: 105},
+            {name: 'celery', calories: 45},
+            {name: 'corn', calories: 35},
+            {name: 'broccoli', calories: 37},
+            {name: 'zuccini', calories: 29}
+        ];
+  return (
+    <>
+      {fruits.length > 0 && <List items = {fruits} category = "Fruits" />}
+      {vegetables.length > 0 ? <List items = {vegetables} category = "Veggies" /> : ""}
+    </>
+  )
+}
+
+export default App
