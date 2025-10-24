48d1b0c826a257f7794daf51e66ebcacce8fe606
Saraswati Tiwari <saraswati99Tiwari@gmail.com>
2025-10-24 17:54:24 +0530

    click events explained

---
+++ ReactFoundation/click-events/src/App.jsx
@@ -0,0 +1,17 @@
+import Button from "./Button"
+import Button2 from "./Button2"
+import Profile from "./Profile"
+
+function App() {
+  return (
+    <>
+    <Button/>
+    <hr></hr>
+    <Button2/>
+    <hr></hr>
+    <Profile/>
+    </>
+  )
+}
+
+export default App
