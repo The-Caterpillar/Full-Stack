e62e344c0cf18cbafae2386e127cb5408a477c0e
Saraswati Tiwari <saraswati99Tiwari@gmail.com>
2025-10-25 23:40:45 +0530

    UseState

--- ReactFoundation/react-hooks/src/App.jsx
+++ ReactFoundation/react-hooks/src/App.jsx
@@ -1,33 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* React hooks = 
+- Special functions that allow functional components to use React features without class components (version16)
+- If a function has 'use' in the begining, it's usually a react hook.
+- use: State, Effect, Context, Reducer, Callback, etc.
+
+useState() :
+- A react hook that allows the creation of a stateful variable AND a setter function to update it's value
+in the virtual DOM. [name, setName]
+*/
+
+import MyComponent from "./MyComponent"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <MyComponent/>
     </>
   )
 }
