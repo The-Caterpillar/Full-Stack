4064205f3d871e42b95ffecbd651cf876cd552d1
Saraswati <115623683+The-Caterpillar@users.noreply.github.com>
2025-11-14 19:20:28 +0530

    Use ref hook (#34)
    * understandinf useRef hook
    
    * timer
    
    * create timer - persisting values accross states using useRef
    
    * updated composition of component

--- ReactFoundation/react-Concepts/src/useRef_Timer/Timer.jsx
+++ ReactFoundation/react-Concepts/src/useRef_Timer/Timer.jsx
@@ -1,18 +1,41 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
+import TimerDisplay from "./TimerDisplay";
 const Timer = () => {
     const timeRef = useRef(null);
-    const [time,setTime] = useState(0);
     const [isRunning,setIsRunning] = useState(false);
 
+    const [time,setTime] = useState(() => {
+        return Number(localStorage.getItem('time') || 0)
+    });
+    useEffect(()=>{
+        localStorage.setItem('time',time);
+    },[time])
+
+    function toggleTimer(){
+        if(isRunning){
+            clearInterval(timeRef.current);
+            timeRef.current = null;
+        }
+        else{
+            timeRef.current = setInterval(()=>{
+                setTime((prevTime) => prevTime + 1)
+            },1000);
+        }
+        setIsRunning(!isRunning);
+    }
+
+    // reset timer
+    function resetTimer(){
+        clearInterval(timeRef.current);
+        setIsRunning(false);
+        setTime(0);
+        timeRef.current = null;
+        localStorage.removeItem('time');
+    }
+
     return ( <>
-    <div className="max-w-md mx-auto mt-10 p-6 bg-gray-100 rounded-lg shadow-lg text-center">
-        <h2 className="text-4xl font-semibold mt-4">
-            Timer: {time}
-        </h2>
-        <button className="mt-3 bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600">
-            {isRunning ? 'Pause' : 'Start'}
-        </button>
-    </div>
+    <TimerDisplay time={time} isRunning={isRunning}
+    resetTimer={resetTimer} toggleTimer={toggleTimer}/>
     </> );
 }
 
